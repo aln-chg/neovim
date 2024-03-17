b7cf8887ac67bf7a51f3a738267a76ec269e9dd8
aln-chg <shroudrowdy0203@gmail.com>
2024-03-17 17:02:37 -0500

    initial commit

---
+++ lua/keymaps.lua
@@ -0,0 +1,26 @@
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Map buffer actions
+map("n", "<leader>d", ":bd<cr>", opts)
+map("n", "<leader>p", ":bp<cr>", opts)
+map("n", "<leader>n", ":bn<cr>", opts)
+
+-- Map netrw
+map("n", "<Leader>x", ":Ex<cr>", opts)
+
+-- Disable scary q: history
+map("n", "q:", "<nop>", opts);
+
+-- Move blocks when selected
+map("v", "J", ":m '>+1<cr>gv=gv", opts)
+map("v", "K", ":m '<-2<cr>gv=gv", opts)
+
+-- Cursor stay in one place when using J in normal mode
+map("n", "J", "mzJ`z", opts)
+
+--code format
+map("n", "<leader>cf", ":LspZeroFormat<CR>", opts)
+
+--keymap that copies highlighted line to system clipboard
+map("v", "<leader>y", '"+y', {noremap = true, silent = true})
---
+++ lua/opts.lua
@@ -0,0 +1,42 @@
+vim.g.mapleader = " "
+vim.o.updatetime = 10
+
+-- Gruvbox material setup, for some reason it doesn't work in grubox config block
+vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_foreground = "original"
+vim.g.gruvbox_material_transparent_background = 1
+vim.g.gruvbox_material_ui_contrast = "high"
+
+-- Disable mouse
+vim.o.mouse = ""
+
+-- Use system clipboard
+--vim.o.clipboard = "unnamedplus"
+
+-- Disable error sounds
+vim.o.noerrorbells = true
+vim.o.novisualbell = true
+
+-- Relative line numbering
+vim.o.nu = true
+vim.o.rnu = true
+
+-- Incremntal searching
+vim.o.incsearch = true
+vim.o.smartcase = true
+
+-- For regex turn magic on
+vim.o.magic = true
+
+-- Set the default indent to 4 spaces
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+
+-- No line wrapping
+vim.o.wrap = false
+
+-- Current line highlight
+vim.o.cursorline = true
